2250618044eac08bf40fb213740f1f722b1d7c85
vinitaparote <viniparote1801@gmail.com>
2024-02-12 11:17:18 +0530

    demo

--- routes/JobRouter.js
+++ routes/JobRouter.js
@@ -4,7 +4,7 @@ const router = Router();
 import {getAllJobs, creatJob, getSingleJob, deleteJob, updateJob} from '../Controllers/JobController.js';
 import { validateJobInput, validateIdParam } from "../middleware/validationMiddleware.js"; 
 // router.get('/', getAllJobs);
- // router.post('/', createJob);
+//  router.post('/', createJob);
 
  router.route('/').get(getAllJobs).post(validateJobInput, validateIdParam, creatJob);
  router.route('/:id')
